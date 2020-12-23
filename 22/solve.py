2c319f6f381740b5a056c6bfefb841edc5327f0b
tony chang <tony@ponderer.org>
2020-12-22 20:59:40 -0800

    Clean up the solution to day 22 part 2.

--- 22/solve.py
+++ 22/solve.py
@@ -31,43 +31,37 @@ win = p1 if len(p1) > 0 else p2
 print('part1:', sum((i+1) * v for i, v in enumerate(reversed(win))))
 
 def combat(p1, p2):
-  if len(p1) == 0 or len(p2) == 0:
-    return p1, p2
+  mem = set()
 
-  if p1[0] > len(p1) - 1 or p2[0] > len(p2) - 1:
-    if p1[0] > p2[0]:
+  while len(p1) > 0 and len(p2) > 0:
+    key = (tuple(p1), tuple(p2))
+    if key in mem:
+      p1s, p2s = [1], []
+      break
+    mem.add(key)
+
+    if p1[0] > len(p1) - 1 or p2[0] > len(p2) - 1:
+      if p1[0] > p2[0]:
+        p1 = p1[1:] + [p1[0], p2[0]]
+        p2 = p2[1:]
+      else:
+        p2 = p2[1:] + [p2[0], p1[0]]
+        p1 = p1[1:]
+      continue
+
+    p1s, p2s = p1[1:1+p1[0]], p2[1:1+p2[0]]
+    p1s, p2s = combat(p1s, p2s)
+    if len(p1s) > 0:
       p1 = p1[1:] + [p1[0], p2[0]]
       p2 = p2[1:]
     else:
       p2 = p2[1:] + [p2[0], p1[0]]
       p1 = p1[1:]
-
-    return p1, p2
-
-  p1s, p2s = p1[1:1+p1[0]], p2[1:1+p2[0]]
-  mem = set()
-  while len(p1s) > 0 and len(p2s) > 0:
-    key = (tuple(p1s), tuple(p2s))
-    if key in mem:
-      p1s, p2s = [1], []
-      break
-    mem.add(key)
-    p1s, p2s = combat(p1s, p2s)
-
-  if len(p1s) > 0:
-    p1 = p1[1:] + [p1[0], p2[0]]
-    p2 = p2[1:]
-  else:
-    p2 = p2[1:] + [p2[0], p1[0]]
-    p1 = p1[1:]
-
   return p1, p2
 
 p1 = [int(n) for n in p1_lines.split('\n')[1:]]
 p2 = [int(n) for n in p2_lines.split('\n')[1:]]
-while len(p1) > 0 and len(p2) > 0:
-  p1, p2 = combat(p1, p2)
+p1, p2 = combat(p1, p2)
 
-print(p1, p2)
 win = p1 if len(p1) > 0 else p2
 print('part2:', sum((i+1) * v for i, v in enumerate(reversed(win))))
